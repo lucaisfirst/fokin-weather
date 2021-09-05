128844066486a78308f0be5108347c2cf07b5b49
김하진 <knj9417@gmail.com>
2021-09-05 22:36:24 +0900

    dl

--- App.js
+++ App.js
@@ -5,8 +5,12 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <view style={styles.yellowView}>
+        <Text>Hello</Text>
+      </view>
+      <view style={styles.blueView}>
+        <Text>Hello</Text>
+      </view>
     </View>
   );
 }
@@ -14,8 +18,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "orange",
     alignItems: "center",
     justifyContent: "center",
+  },
+  yellowView: {
+    flex: 1,
+    backgroundColor: "yellow",
+  },
+  blueView: {
+    flex: 1,
+    backgroundColor: "blue",
   },
 });
