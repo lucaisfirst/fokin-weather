9b8db40568196bbe4bfa45806d6ef1525380915d
김하진 <knj9417@gmail.com>
2021-09-13 23:38:33 +0900

    [Add]Modify Align

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.yellowView}>
-        <Text>Hello</Text>
+        <Text>너무 배고픈데</Text>
       </View>
       <View style={styles.blueView}>
-        <Text>Hello</Text>
+        <Text>안녕하세요 김하진입니다!</Text>
       </View>
     </View>
   );
@@ -19,15 +19,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "orange",
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "center",
   },
   yellowView: {
     flex: 1,
     backgroundColor: "yellow",
+    alignItems: "center",
+    justifyContent: "center",
   },
   blueView: {
-    flex: 3,
-    backgroundColor: "blue",
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
